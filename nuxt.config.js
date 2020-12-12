9987d6d75bd60f00275d2ec26217c446cc3ffbd1
eis-dev <edbeisdev@gmail.com>
2020-12-13 02:28:14 +0300

    mis

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,14 +28,21 @@ export default {
         'generate:page': page => {
             const doc = cheerio.load(page.html);
             doc('body script').remove();
-            doc('#__nuxt').replaceWith(doc('#__nuxt').html())
-            doc('#__layout').replaceWith(doc('#__layout').html())
+            doc('#__nuxt').replaceWith(doc('#__nuxt').html());
+            doc('#__layout').replaceWith(doc('#__layout').html());
+            // doc('pre').html(doc(this).html().replace(/\n/g, "thereisblankline"));
             let html = doc.html();
             html = html.replace(/<!---->/g, "");
             html = html.replace(/ data-n-head="ssr"/g, "");
             html = html.replace(/ data-n-head-ssr/g, "");
             html = html.replace(/data-vue-ssr-/g, "");
             html = html.replace(/nuxt/g, "eisdev");
+            html = html.replace(/ eisdev-link-active/g, "");
+            html = html.replace(/ eisdev-link-exact-/g, "");
+            html = html.replace(/\n/g, "");
+            html = html.replace(/  /g, " ");
+            html = html.replace(/  /g, " ");
+            html = html.replace(/  /g, " ");
             page.html = html;
         },
     }
