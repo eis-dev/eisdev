d7e7ebf5561a6688b3fbf27cb57ca956d924b3f6
eis-dev <edbeisdev@gmail.com>
2020-12-13 02:02:20 +0300

    hooks

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const cheerio = require('cheerio');
+
 export default {
     head: {
         title: 'eisdev',
@@ -15,9 +17,26 @@ export default {
         fullTextSearchFields: ['title', 'description'],
         liveEdit: false
     },
-    build: {},
     generate: {
         subFolders: false,
-        fallback: false
+        fallback: false,
+        resourceHints: false
+    },
+    render: { resourceHints: false },
+    build: { resourceHints: false },
+    hooks: {
+        'generate:page': page => {
+            const doc = cheerio.load(page.html);
+            doc('body script').remove();
+            doc('#__nuxt').replaceWith(doc('#__nuxt').html())
+            doc('#__layout').replaceWith(doc('#__layout').html())
+            let html = doc.html();
+            html = html.replace(/<!---->/g, "");
+            html = html.replace(/ data-n-head="ssr"/g, "");
+            html = html.replace(/ data-n-head-ssr/g, "");
+            html = html.replace(/data-vue-ssr-/g, "");
+            html = html.replace(/nuxt/g, "eisdev");
+            page.html = html;
+        },
     }
 }
