fc6f41047411a2b9dd7cc6f8db1d58de2428f4af
eis-dev <edbeisdev@gmail.com>
2020-12-13 17:21:01 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,32 @@ const buildOptions = {
             }
         }
     }
-};
+}
+
+function hook(page) {
+    const doc = cheerio.load(page.html);
+    doc('body script').remove();
+    doc('head link').remove();
+    doc('head').append('<link rel="stylesheet" href="/style.css">');
+    doc('#__nuxt').replaceWith(doc('#__nuxt').html());
+    doc('#__layout').replaceWith(doc('#__layout').html());
+    // doc('pre').html(doc(this).html().replace(/\n/g, "thereisblankline"));
+    let html = doc.html();
+    html = html.replace(/<!---->/g, "");
+    html = html.replace(/ data-n-head="ssr"/g, "");
+    html = html.replace(/ data-n-head-ssr/g, "");
+    html = html.replace(/data-vue-ssr-/g, "");
+    html = html.replace(/nuxt/g, "eisdev");
+    html = html.replace(/eisdev-content/g, "nuxt-content");
+    html = html.replace(/ eisdev-link-active/g, "");
+    html = html.replace(/ eisdev-link-exact-/g, " ");
+    html = html.replace(/\n/g, "");
+    html = html.replace(/  /g, " ");
+    html = html.replace(/  /g, " ");
+    html = html.replace(/> </g, "><");
+    page.html = html;
+    return page
+}
 
 export default {
     head: {
@@ -39,28 +64,5 @@ export default {
     generate: buildOptions,
     render: buildOptions,
     build: buildOptions,
-    hooks: {
-        'generate:page': page => {
-            const doc = cheerio.load(page.html);
-            doc('body script').remove();
-            doc('head link').remove();
-            doc('head').append('<link rel="stylesheet" href="/style.css">');
-            doc('#__nuxt').replaceWith(doc('#__nuxt').html());
-            doc('#__layout').replaceWith(doc('#__layout').html());
-            // doc('pre').html(doc(this).html().replace(/\n/g, "thereisblankline"));
-            let html = doc.html();
-            html = html.replace(/<!---->/g, "");
-            html = html.replace(/ data-n-head="ssr"/g, "");
-            html = html.replace(/ data-n-head-ssr/g, "");
-            html = html.replace(/data-vue-ssr-/g, "");
-            html = html.replace(/nuxt/g, "eisdev");
-            html = html.replace(/ eisdev-link-active/g, "");
-            html = html.replace(/ eisdev-link-exact-/g, " ");
-            html = html.replace(/\n/g, "");
-            html = html.replace(/  /g, " ");
-            html = html.replace(/  /g, " ");
-            html = html.replace(/> </g, "><");
-            page.html = html;
-        },
-    }
+    hooks: {'generate:page': page => hook(page), 'build:page': page => hook(page), 'render:page': page => hook(page)}
 }
