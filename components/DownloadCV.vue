5a19a49d99fa1a6d0b5e65b77de8bf84e7bb65df
eis-dev <62840222+eis-dev@users.noreply.github.com>
2025-01-16 20:06:43 +0300

    Update DownloadCV.vue

--- components/DownloadCV.vue
+++ components/DownloadCV.vue
@@ -1,5 +1,6 @@
 <template>
-    <nuxt-link to="/cv" class="primary-button p-4 mt-5 mb-4" v-text="'Download CV'"/>
+    <a href="/EYUP_ISIK_CV.pdf" target="_blank" class="primary-button p-4 mt-5 mb-4" v-text="'Download CV'"/>
+    <nuxt-link v-if="false" to="/cv" class="primary-button p-4 mt-5 mb-4" v-text="'Download CV'"/>
 </template>
 
 <script>
